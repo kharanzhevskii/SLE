212db083b2068aadd5ab7da0507a667175f19c0e
kharanzhevskii <kharanzhevskii.ne@phystech.edu>
2024-03-24 15:51:18 +0300

    Symmetrical Gauss-Seidel added

--- src/iterative/SGS.h
+++ src/iterative/SGS.h
@@ -4,7 +4,7 @@
 #include <cmath>
 
 template<typename T>
-std::vector<double> gauss_seidel(const CSR<T>& csr, const std::vector<T>& b,const std::vector<T>& x0, const double percision){
+std::vector<double> SGS(const CSR<T>& csr, const std::vector<T>& b, const std::vector<T>& x0, const double percision){
     std::vector<double> x = x0;
     std::vector<double> r(b.size()); 
     double storage = 0;
@@ -20,6 +20,15 @@ std::vector<double> gauss_seidel(const CSR<T>& csr, const std::vector<T>& b,cons
                 }
             }
             x[i] = (b[i] - storage) / csr(i,i);
+        }
+        for (unsigned long int i = 0; i < b.size(); i++){
+            storage = 0;
+            for (unsigned long int j = csr.get_rows()[b.size()-i-1]; j < csr.get_rows()[b.size()-i]; j++){
+                if (csr.get_cols()[j] != b.size()-i-1){
+                    storage += csr.get_values()[j] * x[csr.get_cols()[j]];
+                }
+            }
+            x[b.size()-i-1] = (b[b.size()-i-1] - storage) / csr(b.size()-i-1, b.size()-i-1);
         }
         r = csr * x - b;
         rr = std::sqrt(r * r);
